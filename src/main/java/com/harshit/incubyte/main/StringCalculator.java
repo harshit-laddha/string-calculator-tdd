49f33d8e93b8f2eb4e3f616a8e0c9ad54265021c
devharshitladdha <laddha1956@gmail.com>
2021-02-02 23:32:53 +0530

    Third Commit : Two Numbers

--- src/main/java/com/harshit/incubyte/main/StringCalculator.java
+++ src/main/java/com/harshit/incubyte/main/StringCalculator.java
@@ -4,7 +4,15 @@ public class StringCalculator {
   public static int add(String input) {
     if (input.isEmpty()) {
       return 0;
+    } else if (!input.contains(",")) {
+      return Integer.parseInt(input);
+    } else {
+      int sum = 0;
+      String[] strArr = input.split(",");
+      for (String n : strArr) {
+        sum += Integer.parseInt(n);
+      }
+      return sum;
     }
-    return Integer.parseInt(input);
   }
 }
--- src/test/java/com/harshit/incubyte/main/StringCalculatorTest.java
+++ src/test/java/com/harshit/incubyte/main/StringCalculatorTest.java
@@ -16,4 +16,10 @@ public class StringCalculatorTest {
     String input = "3";
     assertEquals(3, StringCalculator.add(input));
   }
+
+  @Test
+  public void testTwoNumbers() {
+    String input = "1,4";
+    assertEquals(5, StringCalculator.add(input));
+  }
 }
